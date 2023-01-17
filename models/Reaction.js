0ef86f32bf0571fb24c371a28e88f0185d295a54
Bryant Trinh <trinhbryant@gmail.com>
2023-01-17 15:04:07 -0800

    index.js and reaction.js in models finished

---
+++ models/Reaction.js
@@ -0,0 +1,38 @@
+// Add Schema and types into const, we are structuring our reaction schema by doing so
+
+const { Schema, Types } = require('mongoose');
+
+const dateFormat = require('../utils/dateFormat');
+
+// required for schema is reactionID, reactionBody, username, createdAt
+const reactionSchema = new Schema (
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    }
+  },
+  // mongoose document converted to plain javascript object. Getters: true for when document is converted to plain js object, getters will be applied. we are not including id, so id:false.
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+module.exports = reactionSchema;
