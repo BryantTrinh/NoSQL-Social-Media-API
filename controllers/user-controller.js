5d748b10ac6d50d7f221b67d621bc43e2ba91fa0
Bryant Trinh <trinhbryant@gmail.com>
2023-01-17 19:54:38 -0800

    Finished: adding friends to friend list, deleteing users and their thoughts, update user, create user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -47,6 +47,76 @@ const userController = {
 // create a new user
 
   createUser(req, res) {
+    User.create(req.body)
+    .then((dbUserData) => {
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
 
-  }
+// update a user
+
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      {_id: req.params.userId },
+      { $set: req.body, },
+      { runValidators: true, new: true, }     
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ 
+            message: 'No user associated with this id' });
+          }
+          res.json(dbUserData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+      },
+  
+// Delete user and delete their associated thoughts
+
+  deleteUser(req, res) {
+    User.findOneAndDelete(
+      {_id: req.params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({
+            message: 'No user associated with this id' });
+          }
+          return Thought.deleteMany({_id: {
+            $in: dbUserData.thoughts } })'
+          })
+          .then(() => {
+            res.json({ message: 'User has been deleted, along with all their thoughts.' });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+      },
+
+// add friend to your friends list
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      {_id.req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this associated id' });
+      }
+      res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  
 
